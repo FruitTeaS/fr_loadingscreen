d3569d52176e8ce6e2baba9913027a1aa784dfde
FruitTeaS <133534496+FruitTeaS@users.noreply.github.com>
2023-05-14 00:53:11 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,16 +7,16 @@ Config.Title = "OUR TEAM"
 Config.Logo = "assets/logo.png"
 
 Config.Team = [
-    {name: "FruitTea#9543", description: "Projektleitung", image: "assets/person.png"},
-    {name: "FruitTea#9543", description: "Manager", image: "assets/person.png"},
     {name: "FruitTea#9543", description: "Admin", image: "assets/person.png"},
-    {name: "FruitTea#9543", description: "Supporter", image: "assets/person.png"},
+    {name: "FruitTea#9543", description: "Admin", image: "assets/person.png"},
+    {name: "FruitTea#9543", description: "Admin", image: "assets/person.png"},
+    {name: "FruitTea#9543", description: "Admin", image: "assets/person.png"},
 ];
 
 Config.Socials = [
-    {copytext: "FruitScripts", image: "assets/ts3.png", help: "Copy to clipboard", success: "Copied IP"},
+    {copytext: "https://discord.gg/8aZV9DDrj8", image: "assets/website.png", help: "Copy to clipboard", success: "Copied Link"},
     {copytext: "https://discord.gg/8aZV9DDrj8", image: "assets/discord.png", help: "Copy to clipboard", success: "Copied Link"},
-    {copytext: "https://discord.gg/8aZV9DDrj8", image: "assets/tiktok.png", help: "Copy to clipboard", success: "Copied Link"},
+    {copytext: "https://discord.gg/8aZV9DDrj8", image: "assets/youtube.png", help: "Copy to clipboard", success: "Copied Link"},
 ];
 
 Config.Songs = [
@@ -26,9 +26,4 @@ Config.Songs = [
     "music/music4.mp3",
     "music/music5.mp3",
     "music/music6.mp3",
-    "music/music7.mp3",
-    "music/music8.mp3",
-    "music/music9.mp3",
-    "music/music10.mp3",
-    "music/music11.mp3",
 ];
